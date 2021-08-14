506eaddd85004f736c65e3e95b1fbd62f3397acb
Mauro Roberto da Cruz Cunha <mrccunha@gmail.com>
2021-08-14 14:33:32 -0300

    Arquivos descartáveis

--- api/routes/customerWallets.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const controller = require('../controllers/customer-wallets')();
-  
-    app.route('/api/v1/customer-wallets')
-      .get(controller.listCustomerWallets);
-  }
